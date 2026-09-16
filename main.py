dfd55526724220f43973ae3374e8ef8ca70a4977
Oleksii <krog909090@gmail.com>
2026-09-16 14:15:40 +0200

    prod 1.1

--- main.py
+++ main.py
@@ -2,8 +2,15 @@ from Dekodieren import decoder
 from Kodieren import encoder
 
 zeichenkette = "hallo"
-verschiebung = 4
+verschiebung = -5
 
 print ("Originale Zeichenkette:", zeichenkette)
 print ("Verschobene Zeichenkette:", encoder(zeichenkette, verschiebung))
-print ("Dekodierte Zeichenkette:", decoder(encoder(zeichenkette, verschiebung), verschiebung))
+#print ("Dekodierte Zeichenkette:", decoder(encoder(zeichenkette, verschiebung), verschiebung))
+
+
+def brute_force(verschobene_zeichenkette):
+    for i in range(1, 26):
+        print("Verschiebung:", i, "Dekodierte Zeichenkette:", decoder(verschobene_zeichenkette, i))
+
+brute_force(encoder(zeichenkette, verschiebung))

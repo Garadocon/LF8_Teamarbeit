c63361e1a02b378dc378b109e3b07ca58125ec10
Oleksii <krog909090@gmail.com>
2026-09-16 13:39:16 +0200

    funktion decoder

--- Dekodieren.py
+++ Dekodieren.py
@@ -1,13 +1,11 @@
-input_string = input("Geben Sie den zu dekodierenden String ein: ")
-dekodiert = ""
 
-zeichen = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
-for i in input_string:
-    index = zeichen.index(i)
-    index -= 3
-    index = index % len(zeichen)
-    dekodiert += zeichen[index]
+def decoder(zeichenkette, verschiebung):
+    dekodiert = ""
 
-
-
-print("Der dekodierte String lautet: " + dekodiert)
+    zeichen = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+    for i in zeichenkette:
+        index = zeichen.index(i)
+        index -= verschiebung
+        index = index % len(zeichen)
+        dekodiert += zeichen[index]
+    return dekodiert
